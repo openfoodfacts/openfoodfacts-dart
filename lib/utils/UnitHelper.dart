a2787d6bba51aa3d8ce8269f7654952702ea5545
Alexander Schacht <grumpf@gmx.de>
2020-11-20 09:38:23 +0100

    test fixed for getProduct and searchProducts

--- lib/utils/UnitHelper.dart
+++ lib/utils/UnitHelper.dart
@@ -2,7 +2,7 @@ enum Unit { KCAL, KJ, G, MILLI_G, MICRO_G, MILLI_L, L, PERCENT, UNKNOWN }
 
 class UnitHelper {
   static Unit stringToUnit(String s) {
-    if (s == null) {
+    if (s == null || s.isEmpty) {
       return null;
     }
 
