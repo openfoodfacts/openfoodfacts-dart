5bd689ef8ab331e7d864bca12266af754a600edb
mboussaid <mohamed.boussaid@nexthink.com>
2021-02-08 18:27:54 +0100

    Adding Extract ingredients using OCR. Skipping unstable tests.

--- test/api_postRobotoff_test.dart
+++ test/api_postRobotoff_test.dart
@@ -1,26 +1,26 @@
 
 import 'package:flutter_test/flutter_test.dart';
-//import 'package:openfoodfacts/model/Insight.dart';
-//import 'package:openfoodfacts/model/RobotoffQuestion.dart';
+import 'package:openfoodfacts/model/Insight.dart';
+import 'package:openfoodfacts/model/RobotoffQuestion.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
-//import 'package:openfoodfacts/utils/QueryType.dart';
-//import 'test_constants.dart';
+import 'package:openfoodfacts/utils/QueryType.dart';
+import 'test_constants.dart';
 
 void main() {
   group('$OpenFoodAPIClient answer robotoff question', () {
     test('get questions for Noix de Saint-Jacques EN and answer', () async {
-      /*  RobotoffQuestionResult result =
-          await OpenFoodAPIClient.getRobotoffQuestionsForProduct(
-              "0080868000633", "en", TestConstants.TEST_USER,
-              queryType: QueryType.TEST, count: 1);
+      RobotoffQuestionResult result =
+      await OpenFoodAPIClient.getRobotoffQuestionsForProduct(
+          "0080868000633", "en", TestConstants.TEST_USER,
+          queryType: QueryType.TEST, count: 1);
 
       if (result.status == "found") {
         Status postResult = await OpenFoodAPIClient.postInsightAnnotation(
             result.questions[0].insightId,
             InsightAnnotation.YES,
             TestConstants.TEST_USER);
-        expect(postResult.status, "saved");*/
+        expect(postResult.status, "saved");
       }
-    );
-  });
+    });
+  }, skip: "This Group of tests is unstable");
 }
