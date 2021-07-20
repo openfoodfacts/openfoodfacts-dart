cec9f190264a3013c5643e270022e279cdc4f44e
monsieurtanuki <fabrice_fontaine@hotmail.com>
2021-07-20 09:32:29 +0200

    fix/#189 - simplified time-out prone search products tests
    Impacted file:
    * `api_searchProducts_test.dart`: uncommented test "search products by keywords" with a less verbose keyword; fixed test "search products filter additives" with a less verbose keyword
    * `ContainsAdditives`: added a comment about the probable bad naming of parameter "filter"

--- lib/model/parameter/ContainsAdditives.dart
+++ lib/model/parameter/ContainsAdditives.dart
@@ -8,6 +8,10 @@ class ContainsAdditives extends Parameter {
   @override
   String getValue() => filter ? 'without' : '';
 
+  // TODO: find a less misleading parameter name than "filter"
+  // it looks like the filter is:
+  // * `ContainsAdditives(filter: true)` means "without additives"
+  // * `ContainsAdditives(filter: false)` means "no additive filter at all"
   final bool filter;
 
   const ContainsAdditives({required this.filter});
