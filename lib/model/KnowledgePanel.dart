7c9a7c1e75ab5dd30900aa0658a7d5ac1a1b4c8d
Jasmeet Singh <jasmeetsingh@google.com>
2021-08-26 22:42:43 +0200

    documentation fix

--- lib/model/KnowledgePanel.dart
+++ lib/model/KnowledgePanel.dart
@@ -29,9 +29,9 @@ enum Grade {
 // NOTE: This is WIP, do not use and expect changes.
 @JsonSerializable()
 class KnowledgePanel extends JsonObject {
-  // Each KnowledgePanel has a unique id. Example - "ingredient_origin".
+  // Each KnowledgePanel has a unique id. Example - "ecoscore".
   final String id;
-  // The topics discussed in this knowledge panel
+  // The topics discussed in this knowledge panel, example: "Environment".
   final List<String> topics;
   // Title of the panel. Example - "Eco score D".
   final String title;
@@ -40,10 +40,11 @@ class KnowledgePanel extends JsonObject {
   // URL of an icon representing the Panel.
   @JsonKey(name: 'icon_url')
   final String iconUrl;
-  // Level of this KnowledgePanel. Client may choose to display the panel based on the level.
+  // Level of this KnowledgePanel. Client may choose to display the panel based
+  // on the level.
   final Level level;
-  // KnowledgePanelElement is a single unit of KnowledgePanel that can be rendered
-  // on the client.
+  // KnowledgePanelElement is a single unit of KnowledgePanel that can be
+  // rendered on the client.
   final List<KnowledgePanelElement> elements;
   // Score panels have a grade associated with the Knowledge panel.
   // Scale: 'A' -> 'E'
