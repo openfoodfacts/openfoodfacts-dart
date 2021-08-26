f60491ee0761d75656af2fd991b285dfa020b63a
Jasmeet Singh <jasmeetsingh@google.com>
2021-08-26 16:25:37 +0200

    Add documentation for KnowledgePanel and KnowledgePanelLayout

--- lib/model/KnowledgePanel.dart
+++ lib/model/KnowledgePanel.dart
@@ -19,9 +19,13 @@ enum Relevance {
 // NOTE: This is WIP, do not use and expect changes.
 @JsonSerializable()
 class KnowledgePanel extends JsonObject {
+  // Each KnowledgePanel has a unique id. Example - "ingredient_origin".
   final String id;
+  // Title of the panel. Example - "Origin of ingredients".
   final String title;
+  // Level of relevance of this KnowledgePanel for the Product's eco details.
   final Relevance relevance;
+  // Layout detailing how the KnowledgePanel can be rendered on the client.
   final KnowledgePanelLayout layout;
 
   const KnowledgePanel(
--- lib/model/KnowledgePanelLayout.dart
+++ lib/model/KnowledgePanelLayout.dart
@@ -3,49 +3,39 @@ import '../interface/JsonObject.dart';
 
 part 'KnowledgePanelLayout.g.dart';
 
+// Layout detailing how the KnowledgePanel can be rendered on the client.
 @JsonSerializable()
-class DescriptionUnit extends JsonObject {
-  final String html;
+class KnowledgePanelLayout extends JsonObject {
+  // A layout composes of several KnowledgePanelElements. Each element is
+  // rendered on the Client, all elements together form the KnowledgePanel.
+  final List<KnowledgePanelElement> elements;
 
-  const DescriptionUnit({required this.html});
+  const KnowledgePanelLayout({required this.elements});
 
-  factory DescriptionUnit.fromJson(Map<String, dynamic> json) =>
-      _$DescriptionUnitFromJson(json);
+  factory KnowledgePanelLayout.fromJson(Map<String, dynamic> json) =>
+      _$KnowledgePanelLayoutFromJson(json);
 
   @override
-  Map<String, dynamic> toJson() => _$DescriptionUnitToJson(this);
+  Map<String, dynamic> toJson() => _$KnowledgePanelLayoutToJson(this);
 }
 
-@JsonSerializable()
-class ImageUnit extends JsonObject {
-  final String url;
-  final int? width;
-  final int? height;
-  @JsonKey(name: 'alt_text')
-  final String? altText;
-
-  const ImageUnit({required this.url, this.width, this.height, this.altText});
-
-  factory ImageUnit.fromJson(Map<String, dynamic> json) =>
-      _$ImageUnitFromJson(json);
-
-  @override
-  Map<String, dynamic> toJson() => _$ImageUnitToJson(this);
-}
-
-enum ElementStyle {
-  CONTINUOUS,
-  SEPARATE,
-}
-
+// KnowledgePanelElement is a single unit of KnowledgePanel that can be rendered
+// on the client. An Element must contain exactly one unit of KnowledgePanel
+// (example: Description or Image), having more than one unit will cause an
+// exception.
 @JsonSerializable()
 class KnowledgePanelElement extends JsonObject {
+  // ElementStyle tells the client how to display the element, whether in
+  // a continuous sequence after the last element that was rendered or
+  // separately (left, bottom, right, top) as per the client's discretion.
   @JsonKey(name: 'element_style')
   final ElementStyle elementStyle;
 
+  // Description of the Knowledge panel.
   @JsonKey(name: 'description_unit')
   final DescriptionUnit? descriptionUnit;
 
+  // Image that represents the KnowledgePanel.
   @JsonKey(name: 'image_unit')
   final ImageUnit? imageUnit;
 
@@ -58,18 +48,24 @@ class KnowledgePanelElement extends JsonObject {
   @override
   Map<String, dynamic> toJson() => _validatedKnowledgeElementToJson(this);
 
+  // Convert json -> KnowledgePanelElement and validate that only one unit of
+  // information is present in the KnowledgePanelElement.
   static KnowledgePanelElement _validatedKnowledgeElementPanelFromJson(
       Map<String, dynamic> json) {
     KnowledgePanelElement element = _$KnowledgePanelElementFromJson(json);
     return _validateKnowledgePanelElementsOneOf(element);
   }
 
+  // Validate that only one unit of information is present in the
+  // KnowledgePanelElement and then convert KnowledgePanelElement -> json
   static Map<String, dynamic> _validatedKnowledgeElementToJson(
       KnowledgePanelElement element) {
     return _$KnowledgePanelElementToJson(
         _validateKnowledgePanelElementsOneOf(element));
   }
 
+  // Validate that only one unit of information is present in the
+  // KnowledgePanelElement
   static KnowledgePanelElement _validateKnowledgePanelElementsOneOf(
       KnowledgePanelElement element) {
     int aliveUnits = 0;
@@ -87,15 +83,52 @@ class KnowledgePanelElement extends JsonObject {
   }
 }
 
+// ElementStyle tells the client how to display the element.
+enum ElementStyle {
+  // Continue laying out the element in order, render after the last rendered
+  // element.
+  CONTINUOUS,
+  // Display separately from the cluster of "continuous elements". Can be
+  // displayed on the left, bottom, right, top of the entire Panel. Up to the
+  // discretion of the client.
+  SEPARATE,
+}
+
+// Description of the Knowledge panel.
 @JsonSerializable()
-class KnowledgePanelLayout extends JsonObject {
-  final List<KnowledgePanelElement> elements;
+class DescriptionUnit extends JsonObject {
+  // HTML description of one Knowledge Panel Unit.
+  final String html;
 
-  const KnowledgePanelLayout({required this.elements});
+  const DescriptionUnit({required this.html});
 
-  factory KnowledgePanelLayout.fromJson(Map<String, dynamic> json) =>
-      _$KnowledgePanelLayoutFromJson(json);
+  factory DescriptionUnit.fromJson(Map<String, dynamic> json) =>
+      _$DescriptionUnitFromJson(json);
 
   @override
-  Map<String, dynamic> toJson() => _$KnowledgePanelLayoutToJson(this);
+  Map<String, dynamic> toJson() => _$DescriptionUnitToJson(this);
+}
+
+// Image that represents the KnowledgePanel.
+@JsonSerializable()
+class ImageUnit extends JsonObject {
+  // Url of the image.
+  final String url;
+  // Width of the image, this is just a suggestion coming from the server, the
+  // client may choose to use it's own dimensions for the image.
+  final int? width;
+  // Height of the image, this is just a suggestion coming from the server, the
+  // client may choose to use it's own dimensions for the image.
+  final int? height;
+  // Alt Text of the image.
+  @JsonKey(name: 'alt_text')
+  final String? altText;
+
+  const ImageUnit({required this.url, this.width, this.height, this.altText});
+
+  factory ImageUnit.fromJson(Map<String, dynamic> json) =>
+      _$ImageUnitFromJson(json);
+
+  @override
+  Map<String, dynamic> toJson() => _$ImageUnitToJson(this);
 }
