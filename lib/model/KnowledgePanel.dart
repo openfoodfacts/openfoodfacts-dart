5d1538d565cfbbe5a3ceaf6ab2f378754560d364
Jasmeet Singh <jasmeetsingh@google.com>
2021-10-12 11:50:14 +0200

    Make elements nullable

--- lib/model/KnowledgePanel.dart
+++ lib/model/KnowledgePanel.dart
@@ -112,7 +112,7 @@ class KnowledgePanel extends JsonObject {
     required this.parentPanelId,
     required this.titleElement,
     required this.level,
-    required this.elements,
+    this.elements,
     this.type,
     this.topics,
     this.grade,
