6232a2b30657bff2e057a9068bbf4d593801bbc9
vikash singh <46121493+vik4114@users.noreply.github.com>
2022-03-22 10:59:38 +0530

    Fix format

--- lib/model/Nutriments.dart
+++ lib/model/Nutriments.dart
@@ -68,9 +68,7 @@ class Nutriments extends JsonObject {
 
   /// Energy, in kj, per serving
   @JsonKey(
-      name: 'energy-kj',
-      includeIfNull: false,
-      fromJson: JsonObject.parseDouble)
+      name: 'energy-kj', includeIfNull: false, fromJson: JsonObject.parseDouble)
   double? energyKj;
 
   /// Energy, in kcal, per 100 grams of food
