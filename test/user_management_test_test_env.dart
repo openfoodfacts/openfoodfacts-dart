83ead810f4661d0e35cf3c906d9c1ca41713f36a
Marvin M <39344769+M123-dev@users.noreply.github.com>
2022-09-10 20:12:12 +0200

    Fixes

--- test/user_management_test_test_env.dart
+++ test/user_management_test_test_env.dart
@@ -2,44 +2,36 @@ import 'dart:math';
 
 import 'package:openfoodfacts/model/SignUpStatus.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
-import 'package:openfoodfacts/utils/OpenFoodAPIConfiguration.dart';
 import 'package:openfoodfacts/utils/QueryType.dart';
 import 'package:test/test.dart';
 
 void main() {
-  OpenFoodAPIConfiguration.globalQueryType = QueryType.TEST;
+  const QueryType user_test_queryType = QueryType.TEST;
 
   group('Create user', () {
-    test('Login', () async {
+    test('Create user', () async {
       late String email;
       late String name;
       String password = "ThisIsThePassword";
 
       bool shouldRetry = true;
       int counter = 0;
-      const int max_tries = 1;
+      const int max_tries = 5;
 
       while (shouldRetry && counter < max_tries) {
-        print("Try $counter");
-
         counter++;
         name = _generateRandomString(10);
         email = "$name@gmail.com";
 
         print(name);
-        print(email);
 
         SignUpStatus response = await OpenFoodAPIClient.register(
           user: User(userId: name, password: password),
           name: name,
           email: email,
-          queryType: QueryType.TEST,
+          queryType: user_test_queryType,
           newsletter: false,
         );
-
-        print(response.status);
-        print(response.statusErrors);
-        print(response.error);
 
         if (response.status == 201) {
           shouldRetry = false;
@@ -48,9 +40,10 @@ void main() {
 
       bool response = await OpenFoodAPIClient.login(
         User(userId: name, password: password),
-        queryType: QueryType.TEST,
+        queryType: user_test_queryType,
       );
       expect(response, true);
+      print('Creating a account and logging in worked in $counter tries');
     });
   });
 }
@@ -58,5 +51,6 @@ void main() {
 String _generateRandomString(int length) {
   final Random r = Random();
   return String.fromCharCodes(
-      List.generate(length, (index) => r.nextInt(26) + 65));
+    List.generate(length, (index) => r.nextInt(26) + 65),
+  ).toLowerCase();
 }
