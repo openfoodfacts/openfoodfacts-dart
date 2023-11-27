046c4189a116fdd20b2e2100def665f07094a786
monsieurtanuki <fabrice_fontaine@hotmail.com>
2023-11-27 10:13:58 +0100

    feat: 620 - autocomplete for all taxonomy names and fuzziness levels (#835)
    Impacted files:
    * `api_search_test.dart`: added tests for all taxonomy names and all fuzziness levels
    * `fuzziness_level.dart`: added all fuzziness values
    * `open_food_search_api_client.dart`: added a comment
    * `taxonomy_name.dart`: added all taxonomy names

--- lib/src/search/fuzziness_level.dart
+++ lib/src/search/fuzziness_level.dart
@@ -1,9 +1,13 @@
 import 'package:openfoodfacts/src/model/off_tagged.dart';
 
 /// Fuzziness Level for Elastic Search API.
+///
+/// Levenshtein distance (= number of edits).
+/// cf. https://www.elastic.co/guide/en/elasticsearch/reference/current/common-options.html#fuzziness
 enum Fuzziness implements OffTagged {
-  // TODO(monsieurtanuki): introduce other values when available, like 1 and 2.
-  none(offTag: '0');
+  none(offTag: '0'),
+  one(offTag: '1'),
+  two(offTag: '2');
 
   const Fuzziness({
     required this.offTag,
--- lib/src/search/taxonomy_name.dart
+++ lib/src/search/taxonomy_name.dart
@@ -1,9 +1,37 @@
 import '../model/off_tagged.dart';
 
 /// Taxonomy Name for Elastic Search API.
+///
+/// cf. https://github.com/openfoodfacts/search-a-licious/blob/main/data/config/openfoodfacts.yml
 enum TaxonomyName implements OffTagged {
-  // TODO(monsieurtanuki): add other values when available.
-  category(offTag: 'category');
+  category(offTag: 'category'),
+  label(offTag: 'label'),
+  additive(offTag: 'additive'),
+  allergen(offTag: 'allergen'),
+  aminoAcid(offTag: 'amino_acid'),
+  country(offTag: 'country'),
+  dataQuality(offTag: 'data_quality'),
+  foodGroup(offTag: 'food_group'),
+  improvement(offTag: 'improvement'),
+  ingredient(offTag: 'ingredient'),
+  ingredientAnalysis(offTag: 'ingredients_analysis'),
+  ingredientProcessing(offTag: 'ingredients_processing'),
+  language(offTag: 'language'),
+  mineral(offTag: 'mineral'),
+  misc(offTag: 'misc'),
+  novaGroup(offTag: 'nova_group'),
+  nucleotide(offTag: 'nucleotide'),
+  nutrient(offTag: 'nutrient'),
+  origin(offTag: 'origin'),
+  otherNutritionalSubstance(offTag: 'other_nutritional_substance'),
+  packagingMaterial(offTag: 'packaging_material'),
+  packagingRecycling(offTag: 'packaging_recycling'),
+  packagingShape(offTag: 'packaging_shape'),
+  periodsAfterOpening(offTag: 'periods_after_opening'),
+  preservation(offTag: 'preservation'),
+  state(offTag: 'state'),
+  vitamin(offTag: 'vitamin'),
+  brand(offTag: 'brand');
 
   const TaxonomyName({
     required this.offTag,
