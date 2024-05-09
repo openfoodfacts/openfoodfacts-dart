37fd6122cbc16a7b8922f269da920169c1ada976
monsieurtanuki <fabrice_fontaine@hotmail.com>
2024-05-09 16:20:13 +0200

    feat: 922 - default currency for each country (#923)
    * feat: 922 - default currency for each country
    
    Impacted files:
    * `api_off_tag_test.dart`: added a test about the relationship between country and currency
    * `country_helper.dart`: added a `Currency` field to `OpenFoodFactsCountry`
    * `currency.dart`: added fields in order to detect the most important currencies; added comments.
    
    * typo fix

--- test/api_off_tag_test.dart
+++ test/api_off_tag_test.dart
@@ -22,4 +22,56 @@ void main() {
       expect(OpenFoodFactsCountry.fromOffTag(entry.key), entry.value);
     }
   });
+
+  test('$OpenFoodAPIClient country/currency', () async {
+    final Set<Currency> countryCurrencies = <Currency>{};
+    for (final OpenFoodFactsCountry country in OpenFoodFactsCountry.values) {
+      final Currency? currency = country.currency;
+      if (currency == null) {
+        expect(country, OpenFoodFactsCountry.ANTARCTICA);
+        continue;
+      }
+      countryCurrencies.add(currency);
+    }
+    for (final Currency currency in Currency.values) {
+      if (countryCurrencies.contains(currency)) {
+        expect(
+          currency.complementaryCurrency,
+          isFalse,
+          reason: 'Problem with currency $currency',
+        );
+        expect(
+          currency.unofficialCode,
+          isFalse,
+          reason: 'Problem with currency $currency',
+        );
+        expect(
+          currency.historicalCode,
+          isFalse,
+          reason: 'Problem with currency $currency',
+        );
+        expect(
+          currency.fundsCode,
+          isFalse,
+          reason: 'Problem with currency $currency',
+        );
+        expect(
+          currency.noCountry,
+          isFalse,
+          reason: 'Problem with currency $currency',
+        );
+
+        continue;
+      }
+      expect(
+        currency.complementaryCurrency ||
+            currency.unofficialCode ||
+            currency.historicalCode ||
+            currency.fundsCode ||
+            currency.noCountry,
+        isTrue,
+        reason: 'Problem with currency $currency',
+      );
+    }
+  });
 }
