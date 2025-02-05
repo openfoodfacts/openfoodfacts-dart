8f3fd4f1c0830fd4d0ce1f512694cfe549e44d4a
Valimp <valentinchauvet.contact@gmail.com>
2025-02-05 14:10:09 +0100

    fix: delete update class

--- lib/src/nutripatrol/create_flag.dart
+++ lib/src/nutripatrol/create_flag.dart
@@ -11,53 +11,9 @@ class CreateNutripatrolFlag extends JsonObject {
   @JsonKey()
   late NutripatrolTicket ticket;
 
-  /// Barcode of the product.
+  /// id of the flag.
   @JsonKey()
-  String? barcode;
-
-  /// Type of the flag
-  @JsonKey()
-  late String type;
-
-  /// Url of the product
-  @JsonKey()
-  late String url;
-
-  /// User ID
-  @JsonKey(name: 'user_id')
-  late String userId;
-
-  /// Device ID
-  @JsonKey(name: 'device_id')
-  late String deviceId;
-
-  /// Source of the flag
-  @JsonKey()
-  late String source;
-
-  /// Confidence of the flag
-  @JsonKey()
-  double? confidence;
-
-  /// Image ID of the flag
-  @JsonKey(name: 'image_id')
-  String? imageId;
-
-  /// Flavor of the flag
-  @JsonKey()
-  late String flavor;
-
-  /// Reason for the flag
-  @JsonKey()
-  String? reason;
-
-  /// Comment associated with the flag
-  @JsonKey()
-  String? comment;
-
-  /// Created date of the flag
-  @JsonKey(name: 'created_at')
-  late DateTime createdAt;
+  late int id;
 
   CreateNutripatrolFlag();
 
--- lib/src/nutripatrol/create_flag.g.dart
+++ lib/src/nutripatrol/create_flag.g.dart
@@ -11,33 +11,11 @@ CreateNutripatrolFlag _$CreateNutripatrolFlagFromJson(
     CreateNutripatrolFlag()
       ..ticket =
           NutripatrolTicket.fromJson(json['ticket'] as Map<String, dynamic>)
-      ..barcode = json['barcode'] as String?
-      ..type = json['type'] as String
-      ..url = json['url'] as String
-      ..userId = json['user_id'] as String
-      ..deviceId = json['device_id'] as String
-      ..source = json['source'] as String
-      ..confidence = (json['confidence'] as num?)?.toDouble()
-      ..imageId = json['image_id'] as String?
-      ..flavor = json['flavor'] as String
-      ..reason = json['reason'] as String?
-      ..comment = json['comment'] as String?
-      ..createdAt = DateTime.parse(json['created_at'] as String);
+      ..id = (json['id'] as num).toInt();
 
 Map<String, dynamic> _$CreateNutripatrolFlagToJson(
         CreateNutripatrolFlag instance) =>
     <String, dynamic>{
       'ticket': instance.ticket,
-      'barcode': instance.barcode,
-      'type': instance.type,
-      'url': instance.url,
-      'user_id': instance.userId,
-      'device_id': instance.deviceId,
-      'source': instance.source,
-      'confidence': instance.confidence,
-      'image_id': instance.imageId,
-      'flavor': instance.flavor,
-      'reason': instance.reason,
-      'comment': instance.comment,
-      'created_at': instance.createdAt.toIso8601String(),
+      'id': instance.id,
     };
