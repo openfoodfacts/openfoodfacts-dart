00546ee63d7b7593ae397c2d868560e0203b12e3
chetanr25 <1ds22ai010@dsce.edu.com>
2025-04-21 19:21:34 +0530

    refactor: Replaced JsonSerializable with direct JSON handling, removed auto generated price_user.g.dart file

--- lib/src/prices/price_user.dart
+++ lib/src/prices/price_user.dart
@@ -1,71 +1,46 @@
-import 'package:json_annotation/json_annotation.dart';
-
 import '../interface/json_object.dart';
-
-part 'price_user.g.dart';
 
 /// Price user object.
 ///
 /// cf. `User`  in https://prices.openfoodfacts.org/api/docs
-@JsonSerializable()
 class PriceUser extends JsonObject {
-  @JsonKey(name: 'user_id')
-  late String userId;
+  final Map<String, dynamic> json;
+
+  PriceUser(this.json);
+
+  factory PriceUser.fromJson(Map<String, dynamic> json) => PriceUser(json);
+
+  String get userId => json['user_id'] as String;
 
   /// Number of prices for this user.
-  @JsonKey(name: 'price_count')
-  int? priceCount;
+  int? get priceCount => getInt('price_count');
 
   /// Number of locations for this user.
-  @JsonKey(name: 'location_count')
-  int? locationCount;
+  int? get locationCount => getInt('location_count');
 
   /// Number of products for this user.
-  @JsonKey(name: 'product_count')
-  int? productCount;
+  int? get productCount => getInt('product_count');
 
   /// Number of proofs for this user.
-  @JsonKey(name: 'proof_count')
-  int? proofCount;
+  int? get proofCount => getInt('proof_count');
 
-  @JsonKey(name: 'price_type_product_count')
-  int? priceTypeProductCount;
+  int? get priceTypeProductCount => getInt('price_type_product_count');
+  int? get priceTypeCategoryCount => getInt('price_type_category_count');
+  int? get priceKindCommunityCount => getInt('price_kind_community_count');
+  int? get priceKindConsumptionCount => getInt('price_kind_consumption_count');
+  int? get priceCurrencyCount => getInt('price_currency_count');
+  int? get priceInProofOwnedCount => getInt('price_in_proof_owned_count');
+  int? get priceInProofNotOwnedCount =>
+      getInt('price_in_proof_not_owned_count');
+  int? get priceNotOwnedInProofOwnedCount =>
+      getInt('price_not_owned_in_proof_owned_count');
+  int? get proofKindCommunityCount => getInt('proof_kind_community_count');
+  int? get proofKindConsumptionCount => getInt('proof_kind_consumption_count');
+  int? get locationTypeOsmCountryCount =>
+      getInt('location_type_osm_country_count');
 
-  @JsonKey(name: 'price_type_category_count')
-  int? priceTypeCategoryCount;
-
-  @JsonKey(name: 'price_kind_community_count')
-  int? priceKindCommunityCount;
-
-  @JsonKey(name: 'price_kind_consumption_count')
-  int? priceKindConsumptionCount;
-
-  @JsonKey(name: 'price_currency_count')
-  int? priceCurrencyCount;
-
-  @JsonKey(name: 'price_in_proof_owned_count')
-  int? priceInProofOwnedCount;
-
-  @JsonKey(name: 'price_in_proof_not_owned_count')
-  int? priceInProofNotOwnedCount;
-
-  @JsonKey(name: 'price_not_owned_in_proof_owned_count')
-  int? priceNotOwnedInProofOwnedCount;
-
-  @JsonKey(name: 'proof_kind_community_count')
-  int? proofKindCommunityCount;
-
-  @JsonKey(name: 'proof_kind_consumption_count')
-  int? proofKindConsumptionCount;
-
-  @JsonKey(name: 'location_type_osm_country_count')
-  int? locationTypeOsmCountryCount;
-
-  PriceUser();
-
-  factory PriceUser.fromJson(Map<String, dynamic> json) =>
-      _$PriceUserFromJson(json);
+  int? getInt(String key) => json.containsKey(key) ? json[key] as int? : null;
 
   @override
-  Map<String, dynamic> toJson() => _$PriceUserToJson(this);
+  Map<String, dynamic> toJson() => json;
 }
