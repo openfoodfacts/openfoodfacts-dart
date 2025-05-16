89a9358b03cf3cd9aac760920e9849d281110be4
Olaf Görlitz <olaf.goerlitz@gmail.com>
2025-05-16 23:59:39 +0200

    removed dart 3.0 dependency, minor improvements

--- lib/src/model/nutriscore/nutriscore.dart
+++ lib/src/model/nutriscore/nutriscore.dart
@@ -24,6 +24,9 @@ enum NutriScoreInput { category, nutrients, ingredients }
 typedef NutriScore2021 = _NutriScore<NutriScoreCategory2021>;
 typedef NutriScore2023 = _NutriScore<NutriScoreCategory2023>;
 
+/// Private base class for NutriScore.
+/// It is used to define the common properties and methods for both
+/// NutriScore 2021 and NutriScore 2023.
 class _NutriScore<T extends Enum> {
   final NutriScoreGrade? grade;
   final int? score;
@@ -32,58 +35,75 @@ class _NutriScore<T extends Enum> {
   final T? category;
 
   _NutriScore({
-    String? grade,
+    this.grade,
     this.category,
     this.score,
     this.notApplicableCategory,
-    bool hasCategory = false,
-    bool hasNutrients = false,
-  })  : grade = parseGrade(grade ?? ''),
-        missingData = [
-          if (!hasCategory) NutriScoreInput.category,
-          if (!hasNutrients) NutriScoreInput.nutrients,
-        ];
+    this.missingData = const [],
+  }) : assert(
+          (grade != null && score != null && category != null) ||
+              (grade == null && (notApplicableCategory?.isNotEmpty ?? false)) ||
+              (grade == null && missingData.isNotEmpty),
+          'Either NutriScore is computed or not applicable or unknown',
+        );
 
   bool get isComputed => grade != null;
-  bool get isNotApplicable => grade == null && notApplicableCategory != null;
-  bool get isUnknown => grade == null && missingData.isNotEmpty;
+  bool get isNotApplicable => notApplicableCategory?.isNotEmpty ?? false;
+  bool get isUnknown => missingData.isNotEmpty;
+}
 
-  static NutriScoreGrade? parseGrade(String grade) {
-    return switch (grade.toLowerCase()) {
-      'a' => NutriScoreGrade.A,
-      'b' => NutriScoreGrade.B,
-      'c' => NutriScoreGrade.C,
-      'd' => NutriScoreGrade.D,
-      'e' => NutriScoreGrade.E,
-      _ => null,
-    };
+NutriScoreGrade? _parseGrade(String? grade) {
+  switch (grade?.toLowerCase()) {
+    case 'a':
+      return NutriScoreGrade.A;
+    case 'b':
+      return NutriScoreGrade.B;
+    case 'c':
+      return NutriScoreGrade.C;
+    case 'd':
+      return NutriScoreGrade.D;
+    case 'e':
+      return NutriScoreGrade.E;
+    default:
+      return null;
   }
 }
 
+List<NutriScoreInput> _missingData(bool? hasCategory, bool? hasNutrients) => [
+      if (hasCategory != true) NutriScoreInput.category,
+      if (hasNutrients != true) NutriScoreInput.nutrients,
+    ];
+
 extension NutriScoreDetails2021Ext on NutriScoreDetails {
+  /// Returns the NutriScore 2021 as domain model.
   NutriScore2021? get2021() {
     if (nutriScore2021 == null) return null;
 
     return NutriScore2021(
       category: nutriScore2021?.data?.category,
-      grade: nutriScore2021?.grade,
+      grade: _parseGrade(nutriScore2021?.grade),
       score: nutriScore2021?.score,
       notApplicableCategory: nutriScore2021?.notApplicableCategory,
-      hasCategory: nutriScore2021?.categoryAvailable == true,
-      hasNutrients: nutriScore2021?.nutrientsAvailable == true,
+      missingData: _missingData(
+        nutriScore2021?.categoryAvailable,
+        nutriScore2021?.nutrientsAvailable,
+      ),
     );
   }
 
+  /// Returns the NutriScore 2023 as domain model.
   NutriScore2023? get2023() {
     if (nutriScore2023 == null) return null;
 
     return NutriScore2023(
       category: nutriScore2023?.data?.category,
-      grade: nutriScore2023?.grade,
+      grade: _parseGrade(nutriScore2023?.grade),
       score: nutriScore2023?.score,
       notApplicableCategory: nutriScore2023?.notApplicableCategory,
-      hasCategory: nutriScore2023?.categoryAvailable == true,
-      hasNutrients: nutriScore2023?.nutrientsAvailable == true,
+      missingData: _missingData(
+        nutriScore2023?.categoryAvailable,
+        nutriScore2023?.nutrientsAvailable,
+      ),
     );
   }
 }
