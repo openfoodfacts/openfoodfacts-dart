c460a5e7390d2d5a17869b947c42159ccf1948a6
Primaël Quémerais <primael.quemerais@icloud.com>
2026-01-13 17:51:22 +0100

    Added comment to attribute

--- lib/src/model/attribute.dart
+++ lib/src/model/attribute.dart
@@ -119,6 +119,8 @@ class Attribute extends JsonObject {
   final double? match;
   final String? status;
   final String? panelId;
+  // Possible importance values for this attribute (e.g. importnant, not_important, etc.)
+  // Some attributes may have a limited set of possible values. Others ay have any value.
   final Set<String>? values;
 
   @override
