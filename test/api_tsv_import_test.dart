2590b93227bf6ac7b2c9638b5f35386d7c03a207
Aditya Vishwakarma <adityav2707@gmail.com>
2026-04-28 00:24:46 +0530

    test: expand TsvHelper test coverage to 10 cases

--- test/api_tsv_import_test.dart
+++ test/api_tsv_import_test.dart
@@ -117,5 +117,174 @@ void main() {
         ),
       );
     });
+
+    test('maps literal unknown ecoscore to null', () {
+      const expectedBarcode = '1234567890123';
+      const expectedName = 'Test Product';
+      const expectedQuantity = '100g';
+      const expectedBrands = 'Brand';
+      const expectedNutriscore = 'a';
+      const expectedNovaGroup = 1;
+      const rawEcoscoreGrade = 'unknown';
+      final namesRaw =
+          "[{'lang': main, 'text': $expectedName}, {'lang': en, 'text': $expectedName}]";
+      final line = [
+        expectedBarcode,
+        namesRaw,
+        expectedQuantity,
+        expectedBrands,
+        expectedNutriscore,
+        expectedNovaGroup.toString(),
+        rawEcoscoreGrade,
+      ].join('\t');
+
+      final product = TsvHelper().extractTSVLine(line);
+
+      expect(product.ecoscoreGrade, isNull);
+      expect(product.nutriscore, expectedNutriscore);
+      expect(product.novaGroup, expectedNovaGroup);
+    });
+
+    test('returns null productName and languages when name field is empty', () {
+      const rawBarcode = '1234567890123';
+      const rawNamesField = '';
+      const rawQuantity = '100g';
+      const rawBrands = 'Brand';
+      const rawNutriscore = 'a';
+      const rawNovaGroup = '1';
+      const rawEcoscoreGrade = 'b';
+      final line = [
+        rawBarcode,
+        rawNamesField,
+        rawQuantity,
+        rawBrands,
+        rawNutriscore,
+        rawNovaGroup,
+        rawEcoscoreGrade,
+      ].join('\t');
+
+      final product = TsvHelper().extractTSVLine(line);
+
+      expect(product.productName, isNull);
+      expect(product.productNameInLanguages, isNull);
+    });
+
+    test('parses multiple language entries in product name', () {
+      const expectedBarcode = '1234567890123';
+      const expectedMainName = 'Cheese';
+      const expectedFrName = 'Fromage';
+      const expectedQuantity = '200g';
+      const expectedBrands = 'Brand';
+      const rawNutriscore = '';
+      const rawNovaGroup = '';
+      const rawEcoscoreGrade = '';
+      final namesRaw =
+          "[{'lang': main, 'text': $expectedMainName}, {'lang': en, 'text': $expectedMainName}, {'lang': fr, 'text': $expectedFrName}]";
+      final line = [
+        expectedBarcode,
+        namesRaw,
+        expectedQuantity,
+        expectedBrands,
+        rawNutriscore,
+        rawNovaGroup,
+        rawEcoscoreGrade,
+      ].join('\t');
+
+      final product = TsvHelper().extractTSVLine(line);
+
+      expect(product.productName, expectedMainName);
+      expect(product.productNameInLanguages, isNotNull);
+      expect(product.productNameInLanguages!.length, 2);
+      expect(
+        product.productNameInLanguages![OpenFoodFactsLanguage.ENGLISH],
+        expectedMainName,
+      );
+      expect(
+        product.productNameInLanguages![OpenFoodFactsLanguage.FRENCH],
+        expectedFrName,
+      );
+    });
+
+    test('maps non-integer nova group string to null', () {
+      const rawBarcode = '1234567890123';
+      const rawNamesField = '';
+      const rawQuantity = '100g';
+      const rawBrands = 'Brand';
+      const rawNutriscore = '';
+      const rawNovaGroup = 'invalid';
+      const rawEcoscoreGrade = '';
+      final line = [
+        rawBarcode,
+        rawNamesField,
+        rawQuantity,
+        rawBrands,
+        rawNutriscore,
+        rawNovaGroup,
+        rawEcoscoreGrade,
+      ].join('\t');
+
+      final product = TsvHelper().extractTSVLine(line);
+
+      expect(product.novaGroup, isNull);
+    });
+
+    test('all optional fields empty returns all nulls', () {
+      const expectedBarcode = '9876543210987';
+      const rawNamesField = '';
+      const rawQuantity = '';
+      const rawBrands = '';
+      const rawNutriscore = '';
+      const rawNovaGroup = '';
+      const rawEcoscoreGrade = '';
+      final line = [
+        expectedBarcode,
+        rawNamesField,
+        rawQuantity,
+        rawBrands,
+        rawNutriscore,
+        rawNovaGroup,
+        rawEcoscoreGrade,
+      ].join('\t');
+
+      final product = TsvHelper().extractTSVLine(line);
+
+      expect(product.barcode, expectedBarcode);
+      expect(product.productName, isNull);
+      expect(product.productNameInLanguages, isNull);
+      expect(product.quantity, isNull);
+      expect(product.brands, isNull);
+      expect(product.nutriscore, isNull);
+      expect(product.novaGroup, isNull);
+      expect(product.ecoscoreGrade, isNull);
+    });
+
+    test('parses name with no main entry: productName null, languages set', () {
+      const expectedBarcode = '1234567890123';
+      const expectedEnName = 'Pasta';
+      const expectedQuantity = '500g';
+      const expectedBrands = 'Brand';
+      const rawNutriscore = '';
+      const rawNovaGroup = '';
+      const rawEcoscoreGrade = '';
+      final namesRaw = "[{'lang': en, 'text': $expectedEnName}]";
+      final line = [
+        expectedBarcode,
+        namesRaw,
+        expectedQuantity,
+        expectedBrands,
+        rawNutriscore,
+        rawNovaGroup,
+        rawEcoscoreGrade,
+      ].join('\t');
+
+      final product = TsvHelper().extractTSVLine(line);
+
+      expect(product.productName, isNull);
+      expect(product.productNameInLanguages, isNotNull);
+      expect(
+        product.productNameInLanguages![OpenFoodFactsLanguage.ENGLISH],
+        expectedEnName,
+      );
+    });
   });
 }
